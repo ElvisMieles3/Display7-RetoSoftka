92b311e5e34ce44fb2fd2f290fcdef53e72470d8
ElvisMieles3 <elvismieles@gmail.com>
2019-05-08 04:44:41 +0200

    Números representados en arreglos

---
+++ src/sofka/display7/Numbers.java
@@ -0,0 +1,86 @@
+package sofka.display7;
+
+public class Numbers {
+	
+	public Numbers() {}
+	
+	public String[] cero = {
+			" - ",
+			"| |",
+			"   ",
+			"| |",
+			" - "
+	};
+	 
+	String[] uno = {
+			"   ",
+			"  |",
+			"   ",
+			"  |",
+			"   "
+	};
+	
+	String[] dos =  {
+			" - ",
+			"  |",
+			" - ",
+			"|  ",
+			" - "
+	};
+	
+	String[] tres =  {
+			" - ",
+			"  |",
+			" - ",
+			"  |",
+			"- "
+	};
+	
+	String[] Cuatro =  {
+			"   ",
+			"| |",
+			" - ",
+			"  |",
+			"   "
+	                    };
+	String [] Cinco =  {
+		  " - ",
+		  "|  ",
+		  " - ",
+		  "  |",
+		  " - "
+		};
+	String [] Seis = {
+			  " - ",
+			  "|  ",
+			  " - ",
+			  "| |",
+			  " - "
+			};
+	String [] Siete = {
+			  " - ",
+			  "  |",
+			  "   ",
+			  "  |",
+			  "   "
+			};
+	
+	String [] Ocho = {
+			  " - ",
+			  "| |",
+			  " - ",
+			  "| |",
+			  " - "
+			};
+	
+	String [] Nueve = {
+			  " - ",
+			  "| |",
+			  " - ",
+			  "  |",
+			  " - "
+			};
+
+	
+
+}
